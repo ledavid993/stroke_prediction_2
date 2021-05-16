c08d45391d037c298e4762262d55611ab96de11f
ledavid993 <le.david993@gmail.com>
2021-05-16 02:14:45 -0500

    req

--- server/prediction/views.py
+++ server/prediction/views.py
@@ -4,26 +4,22 @@ from rest_framework.response import Response
 from prediction.predict import predict
 
 # Create your views here.
-test = {
-    "gender": "Male",
-    "age": 67,
-    "hypertension": 0,
-    "heart_disease": 0,
-    "ever_married": "Yes",
-    "work_type": "Private",
-    "Residence_type": "Urban",
-    "avg_glucose_level": 228.69,
-    "bmi": 36.6,
-    "smoking_status": "formerly smoked"
-}
-
-[['Male', 67.0, 0, 1, 'Yes', 'Private',
-  'Urban', 228.69, 36.6, 'formerly smoked']]
+# test = {
+#     "gender": "Male",
+#     "age": 67,
+#     "hypertension": 0,
+#     "heart_disease": 0,
+#     "ever_married": "Yes",
+#     "work_type": "Private",
+#     "Residence_type": "Urban",
+#     "avg_glucose_level": 228.69,
+#     "bmi": 36.6,
+#     "smoking_status": "formerly smoked"
+# }
 
 
 @api_view(['GET', 'POST'])
 def api_add(request):
-    sum = 0
     response_dict = {}
     if request.method == 'GET':
         # Do nothing
@@ -36,5 +32,4 @@ def api_add(request):
             x.append(data[key])
         x = [x]
         response_dict = {"predicted": predict(x)}
-        print(response_dict)
     return Response(response_dict, status=status.HTTP_201_CREATED)
